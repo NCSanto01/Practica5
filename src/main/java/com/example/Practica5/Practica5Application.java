fd4add056a2214eafbdaf492a357d1da8a920b06
NCSanto01 <78079809+NCSanto01@users.noreply.github.com>
2022-03-18 19:05:46 +0100

    Update Practica5Application.java

--- src/main/java/com/example/Practica5/Practica5Application.java
+++ src/main/java/com/example/Practica5/Practica5Application.java
@@ -3,6 +3,7 @@ package com.example.Practica5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,10 +14,10 @@ public class Practica5Application {
 		SpringApplication.run(Practica5Application.class, args);
 	}
 
-	@GetMapping
-	public String hello()
+	@RequestMapping("/")
+	public String index()
 	{
-		return "Hello World";
+		return "servicios.html";
 	}
 
 }

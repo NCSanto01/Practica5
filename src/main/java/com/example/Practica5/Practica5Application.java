133586081a57711483cd5e3854d329cd57c82ab7
NCSanto01 <78079809+NCSanto01@users.noreply.github.com>
2022-03-18 19:13:57 +0100

    Update Practica5Application.java

--- src/main/java/com/example/Practica5/Practica5Application.java
+++ src/main/java/com/example/Practica5/Practica5Application.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Practica5Application {
 
 	public static void main(String[] args) {
